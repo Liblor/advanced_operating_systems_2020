6da5467684ee6a91315b1b41a6774355b0007d22
Andrin Bertschi <abertschi@abertschi.ch>
2020-03-16 19:17:44 +0100

    get rid of init paging region in add_region

--- lib/aos/paging.c
+++ lib/aos/paging.c
@@ -82,11 +82,9 @@ errval_t paging_init_state(struct paging_state *st, lvaddr_t start_vaddr,
 {
     // TODO (M4): Implement page fault handler that installs frames when a page fault
     // occurs and keeps track of the virtual address space.
-
     st->slot_alloc = ca;
     st->cap_l0 = cap_l0;
-    struct paging_region *pr = malloc(sizeof(struct paging_region));
-    add_region(st, start_vaddr, 0xffffffffffff, pr);
+    add_region(st, start_vaddr, 0xffffffffffff, NULL);
     return SYS_ERR_OK;
 }
 
