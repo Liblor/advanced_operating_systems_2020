635baa137b0c758703e9818bb2187a9ffe63654d
Christian Leopoldseder <cleopolds@student.ethz.ch>
2020-03-26 22:07:22 +0100

    Merge missed conflict.

--- lib/aos/paging.c
+++ lib/aos/paging.c
@@ -107,13 +107,7 @@ void create_hashtable(collections_hash_table **hashmap) {
  * \param st The struct to be initialized, must not be NULL.
  * \param start_vaddr Virtual address allocation should start at
  *        this address.
-<<<<<<< HEAD
- * \param cap_l0 Reference to the cap of the L1 VNode.
-||||||| add9e18
- * \param pdir Reference to the cap of the L1 VNode.
-=======
- * \param pdir Reference to the cap of the L0 VNode.
->>>>>>> handout/master
+ * \param cap_l0 Reference to the cap of the L0 VNode.
  * \param ca The slot_allocator to be used by the paging state.
  * \return Either SYS_ERR_OK if no error occured or an error
  * indicating what went wrong otherwise.
