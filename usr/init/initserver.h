35936ab38220b61a15cdebdfe969cbe8bd78d1e1
Christian Leopoldseder <cleopolds@student.ethz.ch>
2020-03-27 23:41:53 +0100

    Adapt initserver to use new generic receive handler.

--- usr/init/initserver.h
+++ usr/init/initserver.h
@@ -5,8 +5,8 @@
 
 #define MAX_TOTAL_PAYLOAD_LENGTH 1024
 
-typedef void (* recv_number_callback_t)(struct lmp_chan *, uint64_t numb);
-typedef void (* recv_string_callback_t)(struct lmp_chan *, char *string);
+typedef void (* recv_number_callback_t)(uint64_t numb);
+typedef void (* recv_string_callback_t)(char *string);
 
 enum pending_state {
     EmptyState = 0,
@@ -14,10 +14,6 @@ enum pending_state {
 };
 
 struct initserver_cb_state {
-    uint32_t bytes_received; ///< How much was read from the client already.
-    uint32_t total_length;
-    enum pending_state pending_state;
-    char *string;
 };
 
 errval_t initserver_init(
