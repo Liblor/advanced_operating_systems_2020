4d1d0332c12b30c65f7671c9fd4014fca0138b72
Andrin Bertschi <abertschi@abertschi.ch>
2020-04-02 22:44:43 +0200

    introduce new thread in test

--- usr/faulter/faulter.c
+++ usr/faulter/faulter.c
@@ -2,6 +2,7 @@
 
 #include <aos/aos.h>
 
+__unused
 static void lazy_alloc(void) {
     void *addr;
     size_t reserved = BASE_PAGE_SIZE * 100;
@@ -19,18 +20,46 @@ static void lazy_alloc(void) {
     debug_printf("successfully lazily reserved and fetched %d bytes of memory\n", reserved);
 }
 
+
+
 __unused
 static void fail(void) {
     const char *addr = (char *) VADDR_OFFSET - BASE_PAGE_SIZE;
     printf("Byte at address %p is '%x'\n", addr, *addr);
 }
 
+
+__unused
+static int thread_1(void *args) {
+    debug_printf("hello from thread :) we attempt to fail...\n");
+    __unused struct thread * thr = thread_self();
+//    debug_printf("thread id: %d\n", thr->id);
+//    debug_printf("thread coreid: %d\n", thr->coreid);
+//    lazy_alloc();
+    fail();
+
+    return 0;
+}
+
+
+
+__unused
+static void thread_test(void) {
+    debug_printf("spawning thread...\n");
+    __unused struct thread *t = thread_create_varstack(thread_1, NULL, 1024);
+}
+
 int main(int argc, char *argv[])
 {
     printf("Faulter spawned\n");
+    thread_test();
 
-    lazy_alloc();
-
+    // uncomment the line below to cause error(bug)
 //    fail();
+
+
+
+
+
     return EXIT_SUCCESS;
 }
