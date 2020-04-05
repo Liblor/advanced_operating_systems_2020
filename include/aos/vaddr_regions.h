157d569ff6441151cdaecf3680344d2ae780e5ef
eikendev <raphael@eiken.dev>
2020-04-05 16:53:33 +0200

    Add further node types for paging module

--- include/aos/vaddr_regions.h
+++ include/aos/vaddr_regions.h
@@ -5,9 +5,10 @@
 #include <aos/paging_types.h>
 
 enum nodetype {
-    NodeType_Free,      ///< This region exists and is free
-    NodeType_Allocated, ///< This region exists and is allocated/mapped
-    NodeType_Reserved,  ///< This region is marked, but not yet mapped
+    NodeType_Free,      ///< This region is free
+    NodeType_Reserved,  ///< This region is intended to be used in a paging region, but is not yet allocated
+    NodeType_Allocated, ///< This region is allocated but not yet mapped into the page table
+    NodeType_Mapped,    ///< This region is mapped into the page table
 };
 
 struct vaddr_region {
@@ -24,6 +25,5 @@ errval_t alloc_vaddr_region(struct paging_state *st, lvaddr_t addr, size_t size,
 errval_t free_region(struct paging_state *st, struct vaddr_region *region);
 errval_t reserve_vaddr_region(struct paging_state *st, void **buf, size_t bytes, size_t alignment);
 errval_t is_vaddr_page_reserved(struct paging_state *st, lvaddr_t vaddr);
-
 
 #endif //BF_AOS_PAGING_REGIONS_H
