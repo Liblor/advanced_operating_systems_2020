899656163c7cb539f70bfc1d293cea0c6f9a843e
eikendev <raphael@eiken.dev>
2020-04-26 16:41:51 +0200

    Move struct rpc_message into aos_rpc.h

--- include/aos/aos_rpc.h
+++ include/aos/aos_rpc.h
@@ -41,7 +41,25 @@ enum rpc_message_method {
     Method_Spawn_Process,
 };
 
+enum rpc_message_status {
+    Status_Ok = 0,
+    Spawn_Failed = 1,
+    Process_Get_Name_Failed = 2,
+    Process_Get_All_Pids_Failed = 3,
+    Status_Error = 4,
+};
 
+struct rpc_message_part {
+    uint32_t payload_length; ///< The length of the message.
+    uint16_t status; ///< status / errors
+    uint8_t method;   ///< Method identifier, see enum rpc_message_method
+    char payload[0]; ///< The total payload data of the message.
+} __attribute__((packed));      // due to correct ordering not necessary but explicit is better
+
+struct rpc_message {
+    struct capref cap; ///< Optional cap to exchange, NULL if not set
+    struct rpc_message_part msg;
+};
 
 /**
  * \brief Call this handler on the receive side for grading
