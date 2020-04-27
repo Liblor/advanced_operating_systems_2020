b2125fb0f72f31135011209ba8c34d9f91436427
Christian Leopoldseder <cleopolds@student.ethz.ch>
2020-04-27 23:18:03 +0200

    Migrate serialserver to UMP.

--- usr/init/serialserver.h
+++ usr/init/serialserver.h
@@ -6,8 +6,9 @@
 typedef void (* putchar_callback_t)(char c);
 typedef void (* getchar_callback_t)(char *c);
 
-struct serialserver_cb_state {
-};
+errval_t serialserver_add_client(struct aos_rpc *rpc);
+
+errval_t serialserver_serve_next(void);
 
 errval_t serialserver_init(
     putchar_callback_t putchar_cb,
