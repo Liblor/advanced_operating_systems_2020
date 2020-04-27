cc18037aa74414be507b14255b4259525fa1cc19
Loris Reiff <loris.reiff@liblor.ch>
2020-04-27 16:13:28 +0200

    Monitor forward messages

--- usr/init/monitorserver.c
+++ usr/init/monitorserver.c
@@ -1,6 +1,7 @@
 #include <aos/aos.h>
 #include <aos/aos_rpc.h>
 #include <aos/aos_rpc_lmp.h>
+#include <aos/aos_rpc_lmp_marshal.h>
 
 #include <rpc/server/lmp.h>
 
@@ -8,31 +9,106 @@
 
 static struct rpc_lmp_server server;
 
-static void service_recv_cb(struct rpc_message *msg, void *callback_state, struct aos_rpc *rpc, void *server_state)
+static errval_t reply_error(
+        struct aos_rpc *rpc,
+        enum rpc_message_method method
+)
 {
+    errval_t err;
+    struct rpc_message msg;
+
+    msg.cap = NULL_CAP;
+    msg.msg.method = method;
+    msg.msg.payload_length = 0;
+    msg.msg.status = Status_Error;
+
+    err = aos_rpc_lmp_send_message(rpc, &msg, LMP_SEND_FLAGS_DEFAULT);
+    if (err_is_fail(err)) {
+        DEBUG_ERR(err, "lmp_send_message failed\n");
+        return err;
+    }
+    return SYS_ERR_OK;
+}
+
+static inline errval_t monitor_forward_receive(
+        struct rpc_message *msg,
+        void *callback_state,
+        struct aos_rpc *rpc,
+        void *server_state
+)
+{
+    errval_t err;
+    // TODO: get rpc to urpc-server
+    struct aos_rpc *server_rpc = NULL;
+    struct rpc_message *recv;
+    err = aos_rpc_ump_send_and_wait_recv(
+            server_rpc,
+            msg,
+            &recv
+    );
+    if (err_is_fail(err)) {
+        err = reply_error(rpc, msg->msg.method);
+        goto cleanup;
+    }
+    err = aos_rpc_lmp_send_message(rpc, recv, LMP_SEND_FLAGS_DEFAULT);
+cleanup:
+    if (recv != NULL) {
+        free(recv);
+    }
+    return err;
+}
+
+
+static inline errval_t monitor_forward(
+        struct rpc_message *msg,
+        void *callback_state,
+        struct aos_rpc *rpc,
+        void *server_state
+)
+{
+    errval_t err;
+    // TODO: get rpc to urpc-server
+    struct aos_rpc *server_rpc = NULL;
+    err = aos_rpc_ump_send_message(
+            server_rpc,
+            msg
+    );
+    return err;
+}
+
+static void service_recv_cb(
+        struct rpc_message *msg,
+        void *callback_state,
+        struct aos_rpc *rpc,
+        void *server_state
+)
+{
+    errval_t err = SYS_ERR_OK;
 	switch (msg->msg.method) {
     case Method_Get_Ram_Cap:
-        // TODO: forward msg to mem server via urpc
+        err = monitor_forward_receive(msg, callback_state, rpc, server_state);
         break;
-
     case Method_Send_Number:
     case Method_Send_String:
-        // TODO: forward msg to init server via urpc
+        err = monitor_forward(msg, callback_state, rpc, server_state);
         break;
-
     case Method_Serial_Putchar:
-    case Method_Serial_Getchar:
-        // TODO: forward msg to serial server via urpc
+        err = monitor_forward(msg, callback_state, rpc, server_state);
         break;
-
+    case Method_Serial_Getchar:
+        err = monitor_forward_receive(msg, callback_state, rpc, server_state);
+        break;
     case Method_Process_Get_Name:
     case Method_Process_Get_All_Pids:
     case Method_Spawn_Process:
-        // TODO: forward msg to process server via urpc
+        err = monitor_forward_receive(msg, callback_state, rpc, server_state);
         break;
 
 	default:
-	        debug_printf("monitor server: unknown msg->msg.method given: type: %d\n", msg->msg.method);
+        debug_printf("monitor server: unknown msg->msg.method given: type: %d\n", msg->msg.method);
+	}
+	if (err_is_fail(err)) {
+	    debug_printf("Unhandled error in monitorserver service_recv_cb\n");
 	}
 }
 
