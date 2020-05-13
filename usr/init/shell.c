b22544c2449b284344056b3df82fc53c67dbb8c2
Andrin Bertschi <abertschi@abertschi.ch>
2020-05-13 16:59:37 +0200

    enabling gic driver

--- usr/init/shell.c
+++ usr/init/shell.c
@@ -8,6 +8,7 @@
 #include "shell.h"
 #include <drivers/lpuart.h>
 #include <maps/imx8x_map.h>
+#include <drivers/gic_dist.h>
 
 static errval_t map_device_into_vspace(gensize_t offset, size_t objsize, void **ret_vaddr)
 {
@@ -54,8 +55,9 @@ errval_t shell_init(void)
 {
     errval_t err;
 
-    void * lpuart3_base;
-    err = map_device_into_vspace((IMX8X_UART3_BASE - IMX8X_START_DEV_RANGE), IMX8X_UART_SIZE,
+    void *lpuart3_base;
+    err = map_device_into_vspace((IMX8X_UART3_BASE - IMX8X_START_DEV_RANGE),
+                                 IMX8X_UART_SIZE,
                                  &lpuart3_base);
     if (err_is_fail(err)) {
         debug_printf("paging_map_frame() failed: %s\n", err_getstring(err));
@@ -74,6 +76,8 @@ errval_t shell_init(void)
 
     SHELL_DEBUG("lpuart_init ok\n");
 
+
+#if 0
     char ret;
     do {
         err = lpuart_getchar(lpuart, &ret);
@@ -83,8 +87,31 @@ errval_t shell_init(void)
         debug_printf("lpuart_getchar() failed: %s\n", err_getstring(err));
         return err_push(err, LPUART_ERR_INVALID_DEV);
     }
-
     SHELL_DEBUG("ret: %c\n", ret);
+#endif
+
+    //err = lpuart_enable_interrupt(lpuart);
+
+    void *gic_base;
+    err = map_device_into_vspace((IMX8X_GIC_DIST_BASE - IMX8X_START_DEV_RANGE),
+                                 IMX8X_GIC_DIST_SIZE,
+                                 &gic_base);
+
+    if (err_is_fail(err)) {
+        debug_printf("failed to map gic into vspace: %s\n", err_getstring(err));
+        return err;
+    }
+
+    SHELL_DEBUG("mapped gic at addr %p\n", gic_base);
+
+    struct gic_dist_s *gic_data;
+    err = gic_dist_init(&gic_data, gic_base);
+
+    if (err_is_fail(err)) {
+        debug_printf("failed to call gic_dist_init:  %s\n", err_getstring(err));
+        return err;
+    }
+
 
 
     return SYS_ERR_OK;
