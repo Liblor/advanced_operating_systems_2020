29bf5fdf8c9a2c69cc75334313d47e29af2ca827
Andrin Bertschi <abertschi@abertschi.ch>
2020-05-13 22:00:16 +0200

    enable interrupts in userspace

--- usr/init/shell.c
+++ usr/init/shell.c
@@ -6,9 +6,12 @@
 #include <stdio.h>
 
 #include "shell.h"
-#include <drivers/lpuart.h>
+
 #include <maps/imx8x_map.h>
-#include <drivers/gic_dist.h>
+
+#include <aos/inthandler.h>
+
+struct shell_state shell_state;
 
 static errval_t map_device_into_vspace(gensize_t offset, size_t objsize, void **ret_vaddr)
 {
@@ -51,46 +54,32 @@ static errval_t map_device_into_vspace(gensize_t offset, size_t objsize, void **
     return SYS_ERR_OK;
 }
 
+
+static void lpuart_int(void *arg)
+{
+//    SHELL_DEBUG("receiving interrupt\n");
+
+    errval_t err;
+    char ret;
+    err = lpuart_getchar(shell_state.lpuart3_state, &ret);
+    if (err_is_fail(err)) {
+        debug_printf("lpuart_getchar() failed: %s\n", err_getstring(err));
+        abort();
+    }
+    err = lpuart_putchar(shell_state.lpuart3_state, ret);
+    if (err_is_fail(err)) {
+        debug_printf("lpuart_putchar() failed: %s\n", err_getstring(err));
+        abort();
+    }
+}
+
+
 errval_t shell_init(void)
 {
     errval_t err;
 
-    void *lpuart3_base;
-    err = map_device_into_vspace((IMX8X_UART3_BASE - IMX8X_START_DEV_RANGE),
-                                 IMX8X_UART_SIZE,
-                                 &lpuart3_base);
-    if (err_is_fail(err)) {
-        debug_printf("paging_map_frame() failed: %s\n", err_getstring(err));
-        return err_push(err, LIB_ERR_PAGING_MAP_FRAME);
-    }
+    memset(&shell_state, 0, sizeof(struct shell_state));
 
-    SHELL_DEBUG("ok so far, lpuart3 data at %p\n", lpuart3_base);
-
-    struct lpuart_s *lpuart;
-    err = lpuart_init(&lpuart, lpuart3_base);
-
-    if (err_is_fail(err)) {
-        debug_printf("lpuart_init() failed: %s\n", err_getstring(err));
-        return err_push(err, LPUART_ERR_INVALID_DEV);
-    }
-
-    SHELL_DEBUG("lpuart_init ok\n");
-
-
-#if 0
-    char ret;
-    do {
-        err = lpuart_getchar(lpuart, &ret);
-    } while(err == LPUART_ERR_NO_DATA);
-
-    if (err_is_fail(err)) {
-        debug_printf("lpuart_getchar() failed: %s\n", err_getstring(err));
-        return err_push(err, LPUART_ERR_INVALID_DEV);
-    }
-    SHELL_DEBUG("ret: %c\n", ret);
-#endif
-
-    //err = lpuart_enable_interrupt(lpuart);
 
     void *gic_base;
     err = map_device_into_vspace((IMX8X_GIC_DIST_BASE - IMX8X_START_DEV_RANGE),
@@ -104,14 +93,72 @@ errval_t shell_init(void)
 
     SHELL_DEBUG("mapped gic at addr %p\n", gic_base);
 
-    struct gic_dist_s *gic_data;
-    err = gic_dist_init(&gic_data, gic_base);
+    err = gic_dist_init(&shell_state.gic_dist_state, gic_base);
 
     if (err_is_fail(err)) {
         debug_printf("failed to call gic_dist_init:  %s\n", err_getstring(err));
         return err;
     }
 
+    void *lpuart3_base;
+    err = map_device_into_vspace((IMX8X_UART3_BASE - IMX8X_START_DEV_RANGE),
+                                 IMX8X_UART_SIZE,
+                                 &lpuart3_base);
+    if (err_is_fail(err)) {
+        debug_printf("paging_map_frame() failed: %s\n", err_getstring(err));
+        return err_push(err, LIB_ERR_PAGING_MAP_FRAME);
+    }
+
+    SHELL_DEBUG("ok so far, lpuart3 data at %p\n", lpuart3_base);
+
+    err = lpuart_init(&shell_state.lpuart3_state, lpuart3_base);
+
+    if (err_is_fail(err)) {
+        debug_printf("lpuart_init() failed: %s\n", err_getstring(err));
+        return err_push(err, LPUART_ERR_INVALID_DEV);
+    }
+
+    SHELL_DEBUG("lpuart_init ok\n");
+
+
+#if 0
+    while (1) {
+        char ret;
+        do {
+            err = lpuart_getchar(shell_state.lpuart3_state, &ret);
+        } while (err == LPUART_ERR_NO_DATA);
+        if (err_is_fail(err)) {
+            debug_printf("lpuart_getchar() failed: %s\n", err_getstring(err));
+            abort();
+        }
+        SHELL_DEBUG("%c\n", ret);
+    }
+#endif
+    err = inthandler_alloc_dest_irq_cap(IMX8X_UART3_INT,
+                                        &shell_state.irq_dest_cap);
+    if (err_is_fail(err)) {
+        debug_printf("failed alloc dest irq cap (inthandler_alloc_dest_irq_cap):  %s\n",
+                     err_getstring(err));
+        return err;
+    }
+
+    err = inthandler_setup(shell_state.irq_dest_cap, get_default_waitset(), MKCLOSURE(lpuart_int, NULL));
+    if (err_is_fail(err)) {
+        debug_printf("failed to call inthandler_setup:  %s\n", err_getstring(err));
+        return err;
+    }
+
+    err = gic_dist_enable_interrupt(shell_state.gic_dist_state, IMX8X_UART3_INT, 1, 0);
+    if (err_is_fail(err)) {
+        debug_printf("failed to call gic_dist_enable_interrupt:  %s\n", err_getstring(err));
+        return err;
+    }
+
+    err = lpuart_enable_interrupt(shell_state.lpuart3_state);
+    if (err_is_fail(err)) {
+        debug_printf("failed to call lpuart_enable_interrupt:  %s\n", err_getstring(err));
+        return err;
+    }
 
 
     return SYS_ERR_OK;
--- usr/init/shell.h
+++ usr/init/shell.h
@@ -5,6 +5,9 @@
 #ifndef BFOS_SHELL_H
 #define BFOS_SHELL_H
 
+#include <drivers/lpuart.h>
+#include <drivers/gic_dist.h>
+
 #define SHELL_DEBUG_ON
 #if defined(SHELL_DEBUG_ON)
 #define SHELL_DEBUG(x...) debug_printf("shell:" x)
@@ -12,6 +15,11 @@
 #define SHELL_DEBUG(x...) ((void)0)
 #endif
 
+struct shell_state {
+    struct lpuart_s *lpuart3_state;
+    struct gic_dist_s *gic_dist_state;
+    struct capref irq_dest_cap;
+};
 
 errval_t shell_init(void);
 
