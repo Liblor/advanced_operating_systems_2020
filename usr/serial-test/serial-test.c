a10b8f49469b086ba46e7a544e031f783bbf02d5
Andrin Bertschi <abertschi@abertschi.ch>
2020-05-15 17:34:40 +0200

    add serial test

---
+++ usr/serial-test/serial-test.c
@@ -0,0 +1,91 @@
+/**
+ * \file
+ * \brief Hello world application
+ */
+
+/*
+ * Copyright (c) 2016 ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, CAB F.78, Universitaetstr. 6, CH-8092 Zurich,
+ * Attn: Systems Group.
+ */
+
+
+#include <stdio.h>
+
+#include <aos/aos.h>
+#include <aos/debug.h>
+#include <aos/aos_rpc.h>
+#include <spawn/spawn.h>
+
+const char long_string[] = "this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string"
+                           "this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string"
+                           "this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string"
+                           "this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string"
+                           "this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string"
+                           "this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string"
+                           "this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string"
+                           "this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string this is a very long string\n";
+
+__unused static void test_serial(void) {
+    errval_t err;
+
+    debug_printf("Testing serial RPC...\n");
+
+    struct aos_rpc *rpc = aos_rpc_get_serial_channel();
+    if (rpc == NULL) {
+        debug_printf("Could not create serial channel\n");
+        return;
+    }
+    /*
+    // Explicit test not necessary since printf is redirected to rpc during the
+    // execution of this entire program.
+    err = aos_rpc_lmp_serial_putchar(rpc_serial, 'a');
+    if (err_is_fail(err)) {
+        DEBUG_ERR(err, "aos_rpc_lmp_serial_putchar()");
+        return;
+    }
+    */
+
+    printf("If you see this message and the libc terminal write function is set in lib/aos/init.c it means aos_rpc_lmp_serial_putchar() is working\n");
+    printf("1234567890abcdefghejklmnopqrstuvwxyz\n");
+
+    debug_printf("Press a button to test aos_rpc_lmp_serial_getchar(): ");
+    char c;
+    err = aos_rpc_lmp_serial_getchar(rpc, &c);
+    if (err_is_fail(err)) {
+        DEBUG_ERR(err, "aos_rpc_lmp_serial_getchar()");
+        return;
+    }
+    debug_printf("\n");
+    debug_printf("Received %c\n", c);
+}
+
+__unused
+static void write_simple(void) {
+    debug_printf("Testing write_simple ...\n");
+    struct aos_rpc *rpc = aos_rpc_get_serial_channel();
+    if (rpc == NULL) {
+        debug_printf("Could not create serial channel\n");
+        return;
+    }
+
+    printf("If you see this message and the libc terminal write function is set in lib/aos/init.c it means aos_rpc_lmp_serial_putchar() is working\n");
+    printf("1234567890abcdefghejklmnopqrstuvwxyz\n");
+    printf("%s\n", long_string);
+}
+
+int main(int argc, char *argv[])
+{
+    debug_printf("Running RPC tests...\n");
+
+    // test_serial();
+    write_simple();
+
+    debug_printf("done\n");
+
+    return EXIT_SUCCESS;
+}
