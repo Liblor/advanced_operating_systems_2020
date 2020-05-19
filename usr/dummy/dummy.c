080fadab7c8f4916140a50e8ccee134512c2bd41
Loris Reiff <loris.reiff@liblor.ch>
2020-05-19 17:24:42 +0200

    Init fat32 filesystem

--- usr/dummy/dummy.c
+++ usr/dummy/dummy.c
@@ -2,23 +2,17 @@
 
 #include <aos/aos.h>
 #include <aos/aos_rpc.h>
+#include <aos/fat32.h>
 
 int main(int argc, char *argv[])
 {
     debug_printf("Dummy spawned\n");
-    char buf[512];
-    errval_t err = aos_rpc_block_driver_read_block(aos_rpc_get_block_driver_channel(), 0, buf, 512);
+
+    struct fat32_mnt *mnt;
+    errval_t err = mount_fat32("test", &mnt);
     if (err_is_fail(err)) {
-        DEBUG_ERR(err, "Block driver rpc fail\n");
+        DEBUG_ERR(err, "mount failed\n");
     }
-
-    printf("0x%x\n", buf[0]);
-    assert(buf[0] == 0xeb);
-    //err = aos_rpc_block_driver_write_block(aos_rpc_get_block_driver_channel(), 0, buf, 512);
-    //if (err_is_fail(err)) {
-        //DEBUG_ERR(err, "Block driver rpc fail\n");
-    //}
-
 
     return EXIT_SUCCESS;
 }
