e89921733f28f8b2578140f7795429fc188f5854
eikendev <raphael@eiken.dev>
2020-05-20 15:23:04 +0200

    Add new binary for UDP echo functionality

---
+++ usr/echoserver/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <aos/debug.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Starting %s\n", argv[0]);
+
+    /* TODO: When a UDP datagram is received, echo it to the sender. */
+
+    return EXIT_SUCCESS;
+}
