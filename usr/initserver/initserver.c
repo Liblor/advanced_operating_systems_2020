99edd17d726513ed9938d953ff110813d6907563
Christian Leopoldseder <cleopolds@student.ethz.ch>
2020-05-22 12:16:10 +0200

    Remove copyright notice.

--- usr/initserver/initserver.c
+++ usr/initserver/initserver.c
@@ -1,18 +1,3 @@
-/**
- * \file
- * \brief Hello world application
- */
-
-/*
- * Copyright (c) 2016 ETH Zurich.
- * All rights reserved.
- *
- * This file is distributed under the terms in the attached LICENSE file.
- * If you do not find this file, copies can be found by writing to:
- * ETH Zurich D-INFK, CAB F.78, Universitaetstr. 6, CH-8092 Zurich,
- * Attn: Systems Group.
- */
-
 #include <stdio.h>
 
 #include <aos/aos.h>
