e5220b46b902d5be9fce6f2cbdd07210541e3410
Andrin Bertschi <abertschi@abertschi.ch>
2020-05-22 21:58:42 +0200

    aosh add builtins

---
+++ usr/aosh/builtin/echo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errors/errno.h>
+#include "echo.h"
+#include "../aosh.h"
+
+errval_t builtin_echo(
+        int argc,
+        char **argv)
+{
+    argv++;
+    argc--;
+    char *out = malloc(AOSH_READLINE_MAX_LEN + 1);
+    if (out == NULL) {
+        return LIB_ERR_MALLOC_FAIL;
+    }
+    int b = 0;
+    for (int i = 0; i < argc; i++) {
+        int a = 0;
+        while (*(argv[i] + a) != '\0' && a < AOSH_READLINE_MAX_LEN) {
+            out[b] = *(argv[i] + a);
+            b++;
+            a++;
+        }
+        if (a > 0) {
+            out[b] = ' ';
+            b++;
+        }
+    }
+    out[b] = '\0';
+    printf("%s\n", out);
+    free(out);
+    return SYS_ERR_OK;
+}
