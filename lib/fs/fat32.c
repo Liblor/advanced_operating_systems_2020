144dd313b2baec85495614170df7732ff3c1daa6
Loris Reiff <loris.reiff@liblor.ch>
2020-05-27 17:12:05 +0200

    Add shortname validaton

--- lib/fs/fat32.c
+++ lib/fs/fat32.c
@@ -95,6 +95,36 @@ static inline bool is_shortname(
     return ('A' <= dir_entry->shortname[0] && dir_entry->shortname[0] <= 'Z') || dir_entry->shortname[0] == '.';
 }
 
+static inline bool isalphanum_str(const char *str, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        if (! isalnum(str[i])) { return false; }
+    }
+    return true;
+}
+
+static bool is_valid_name_for_shortname(
+    const char *name
+) {
+    size_t name_len = strnlen(name, 13);
+    if (name_len > 12) { return false; }
+    if (name_len == 0) { return false; }
+    if (name[0] == '.') { return false; }
+    char *dot = strrchr(name, '.');
+    if (dot == NULL) {
+        if (name_len > 8) { return false; }
+        if (! isalphanum_str(name, name_len)) { return false; }
+    } else {
+        size_t len8 = dot - name;
+        dot++;
+        size_t len3 = strnlen(dot, 5);
+        if (len8 > 8) { return false; }
+        if (len3 > 3) { return false; }
+        if (! isalphanum_str(name, len8)) { return false; }
+        if (! isalphanum_str(dot, len3)) { return false; }
+    }
+    return true;
+}
+
 static inline size_t shortname8_len(const char *shortname)
 {
     size_t i;
@@ -1162,6 +1192,8 @@ static errval_t create_data_entry(
         return FS_ERR_NOTFOUND;
     }
     const char *childname = lastsep + 1;
+    if (! is_valid_name_for_shortname) { return FS_ERR_INVALID_SHORTNAME; }
+
     size_t pathlen = lastsep - path;
     char pathbuf[pathlen + 1];
     memcpy(pathbuf, path, pathlen);
@@ -1176,8 +1208,6 @@ static errval_t create_data_entry(
         err = FS_ERR_NOTDIR; // parent is not a directory
         goto cleanup;
     }
-
-    // TODO Validate name
 
     struct fat32_dirent dirent;
     err = get_free_dir_entry(mnt, parent, &dirent);
